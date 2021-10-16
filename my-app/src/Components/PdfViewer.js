f1bd560e968bfd9b87b772fa51c0d1278baee8b0
Pete <peter@pervasent.com>
2021-10-16 15:43:41 -0700

    Added PDF component

---
+++ my-app/src/Components/PdfViewer.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
+
+function PdfViewer(props) {
+  const file = props.file;
+  const [numPages, setNumPages] = useState(null);
+
+  return (
+    <Document
+      file={file}
+      onLoadSuccess={({ numPages }) => setNumPages(numPages)}
+    >
+      {Array.apply(null, Array(numPages))
+        .map((x, i) => i + 1)
+        .map((page) => (
+          <Page pageNumber={page} />
+        ))}
+    </Document>
+  );
+}
+
+export default PdfViewer;
